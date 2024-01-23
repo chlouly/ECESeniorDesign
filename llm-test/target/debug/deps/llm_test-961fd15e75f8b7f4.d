72fd1f8c096998eea344251aabe9ee21db2a9efd
Chris Louly <chrislouly@pal-nat186-105-119.itap.purdue.edu>
2024-01-23 14:47:34 -0500

    Setting up LLM testing in Rust This new branch is used to test LLM loading and running speed in the Rust Programming Language. It will be tested against programs that do the same thing in Python and TypeScript for the sole purpose of choosing the best language for our backend. The blank rust project was set up.

---
+++ llm-test/target/debug/deps/llm_test-961fd15e75f8b7f4.d
@@ -0,0 +1,5 @@
+/Users/chrislouly/Documents/PURDUE/ECE49595/ECESeniorDesign/llm-test/target/debug/deps/llm_test-961fd15e75f8b7f4.rmeta: src/main.rs
+
+/Users/chrislouly/Documents/PURDUE/ECE49595/ECESeniorDesign/llm-test/target/debug/deps/llm_test-961fd15e75f8b7f4.d: src/main.rs
+
+src/main.rs:
