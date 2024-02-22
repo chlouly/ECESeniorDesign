8ae20a1d81c48e8082ca1111037d2f68bcd6c00a
SmenNeoris <140518960+smneoris@users.noreply.github.com>
2024-02-22 15:57:50 -0500

    This is the base of the front end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from 'react';
+import './App.css'; // Assuming you have additional global styles here
+
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
-export default App;
