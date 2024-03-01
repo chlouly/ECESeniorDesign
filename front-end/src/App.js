ee45b47cbf88552e2ba23b95a6497386594e9d6c
SmenNeoris <140518960+smneoris@users.noreply.github.com>
2024-03-01 00:18:35 -0500

    front end update with images

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,16 +5,48 @@ import './App.css'; // Assuming you have additional global styles here
 
 export default function App() {
   return (
-    <div className='bg-black w-screen h-screen grid grid-rows-3 grid-flow-col gap-3 grid grid-columns-3 grid_flow_col gap-3'>
-      <div className='bg-red-500 row-span-2 flex items-center place-content-center'> SAT Questions
-      </div>
+    <div className='bg-black w-screen h-screen grid grid-rows-3 grid-flow-col grid grid-cols-3 grid_flow_col gap-2'>
+      <div className='bg-red-500 row-span-2 flex items-center place-content-center'> SAT Questions</div>
       <div className='bg-blue-500 flex items-center place-content-center'>Answers</div>
-      <div className='bg-green-500 flex items-center place-content-center'>Mosters 1</div>
-      <div className='bg-yellow-500 flex items-center place-content-center'> Helath bar of mosnter one</div>
-      <div className='bg-pink-500 col-span-2 flex items-center place-content-center'>Menu</div>
-      <div className='bg-purple-500 flex items-center place-content-center'>Moster 2</div>
-      <div className='bg-indigo-500 flex items-center place-content-center'>Helath bar mosnter 2</div>
+      <div className="flex items-center justify-center">
+        <a href="#">
+          <div className="w-full h-full">
+            <img className="rounded-lg w-full h-full object-cover" src={require('./baby_yoda.png')} alt="image description" />
+          </div>
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="#">
+          <div className="w-full h-full">
+            <h1 className="text-3xl font-bold text-white">Health bar (baby_yoda)</h1>
+            <img className="rounded-lg w-full h-full object-cover" src={require('./health.jpg')} alt="image description" />
+          </div>
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="#">
+          <div className="w-full h-full">
+            <img className="rounded-lg w-sceen h-full object-cover" src={require('./Menu.png')} alt="image description" />
+          </div>
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="#">
+          <div className="w-full h-full">
+            <img className="rounded-lg w-full h-full object-cover" src={require('./et-feat.jpg')} alt="image description" />
+          </div>
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="#">
+          <div className="w-full h-full">
+            <h1 className="text-3xl font-bold text-white">Health bar (ET)</h1>
+            <img className="rounded-lg w-full h-full object-cover" src={require('./health.jpg')} alt="image description" />
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
+
 
