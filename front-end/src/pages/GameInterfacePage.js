e79a583c21dac5145bf2fedbb606d732dd43f90c
Mateusz Romaniuk <romanczug@icloud.com>
2024-04-23 10:52:18 -0400

    Fix HTTP method in GameInterfacePage.js

--- front-end/src/pages/GameInterfacePage.js
+++ front-end/src/pages/GameInterfacePage.js
@@ -117,7 +117,7 @@ function GameInterface() {
     let user_ID = localStorage.getItem("user_id");
     user_ID = parseInt(user_ID, 10);
     fetch("/waittomove", {
-      method: "POST",
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
